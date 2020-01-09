5de34924010469d5c6340feeb663fedf787f6d3d
Eridanus Sora <sora@sound.moe>
2020-01-09 16:33:05 +0800

    inint

---
+++ index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+const axios = require('axios');
+const url = process.argv[2];
+if (!url) {
+    throw new Error('Please input url');
+}
+const fileId = url.match(/\/s\/(.+)/)[1];
+const transferDetailApi = `https://cowtransfer.com/transfer/transferdetail?url=${fileId}`;
+axios({
+    url: transferDetailApi,
+}).then(transferDetail => {
+    const guid = transferDetail.data.transferFileDtos[0].guid;
+    const filename = transferDetail.data.transferFileDtos[0].guid;
+    const downloadApi = `https://cowtransfer.com/transfer/download?guid=${guid}`;
+    axios({
+        method: 'POST',
+        url: downloadApi,
+        headers: {
+            'Referer': url,
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
+        }
+    }).then(downloadInfo => {
+        console.log(`${downloadInfo.data.link} --header 'referer: ${url}' --header 'authority: static.cowtransfer.com' --header 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36' -O "${filename}"`);
+    });
+})
---
+++ readme.md
@@ -0,0 +1,9 @@
+## Installation
+
+`npm i cowp -g`
+
+## Usage
+
+`cowp "https://cowtransfer.com/s/32a0e8888c1542" | xargs wget`
+
+(Linux only)
